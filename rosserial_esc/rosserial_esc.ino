d8dde628965cfbe294f129fa69ad7badd1e8d135
jmaff <jmaff2003@gmail.com>
2022-09-25 21:48:02 -0400

    Remove boilerplate comment

--- rosserial_esc/rosserial_esc.ino
+++ rosserial_esc/rosserial_esc.ino
@@ -1,15 +1,5 @@
 /*
- * rosserial Servo Control Example
- *
- * This sketch demonstrates the control of hobby R/C servos
- * using ROS and the arduiono
- * 
- * For the full tutorial write up, visit
- * www.ros.org/wiki/rosserial_arduino_demos
- *
- * For more information on the Arduino Servo Library
- * Checkout :
- * http://www.arduino.cc/en/Reference/Servo
+ * rosserial motor controller for BlueRobotics ESC's
  */
 
 #if (ARDUINO >= 100)
