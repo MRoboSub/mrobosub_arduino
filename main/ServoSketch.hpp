1b5c368968e2e44d627f04fd3f2ca70f7c8f5116
Ca7Ac1 <Ca7Ac1@gmail.com>
2023-04-23 15:24:57 -0400

    Allow for multiple servos

--- main/ServoSketch.hpp
+++ main/ServoSketch.hpp
@@ -15,26 +15,41 @@
 namespace ServoSketch
 {
     // Servo Values
-    Servo servo;
-    constexpr int PIN = 12;
+    Servo left_servo;
+    Servo right_servo;
+
+    constexpr int LEFT_PIN = 12;
+    constexpr int RIGHT_PIN = 11;
 
     /* ROSTopic Name */
-    constexpr char *ANGLE_TOPIC_NAME = "/servo/angle";
+    constexpr char *LEFT_ANGLE_TOPIC_NAME = "/left_servo/angle";
+    constexpr char *RIGHT_ANGLE_TOPIC_NAME = "/right_servo/angle";
 
     /* Angle Subscriber */
-    void set_angle(const std_msgs::Int32 &angle_msg)
+    void set_left_angle(const std_msgs::Int32 &angle_msg)
     {
-        servo.write(angle_msg.data);
+        left_servo.write(angle_msg.data);
+    }
+
+    void set_right_angle(const std_msgs::Int32 &angle_msg)
+    {
+        right_servo.write(angle_msg.data);
     }
 
     // NOTE: Angle on servo is restricted between 25 degrees and 155 degrees.
-    ros::Subscriber<std_msgs::Int32> angle_sub(ANGLE_TOPIC_NAME, &set_angle);
+    ros::Subscriber<std_msgs::Int32> left_angle_sub(LEFT_ANGLE_TOPIC_NAME, &set_left_angle);
+    ros::Subscriber<std_msgs::Int32> right_angle_sub(RIGHT_ANGLE_TOPIC_NAME, &set_right_angle);
 
     void setup()
     {
-        servo.attach(PIN);
+        left_servo.attach(LEFT_PIN);
+        right_servo.attach(RIGHT_PIN);
+
         Global::nh.loginfo("Servo attached");
-        Global::nh.subscribe(angle_sub);
+
+        Global::nh.subscribe(left_angle_sub);
+        Global::nh.subscribe(right_angle_sub);
+
         Global::nh.negotiateTopics();
     }
 
