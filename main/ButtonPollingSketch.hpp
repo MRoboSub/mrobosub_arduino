49314f3128d2260796db362bbbad528eac496d2e
iwei20 <44007607+iwei20@users.noreply.github.com>
2023-08-01 09:54:51 -0700

    Fix lack of publisher advertisement

--- main/ButtonPollingSketch.hpp
+++ main/ButtonPollingSketch.hpp
@@ -5,8 +5,8 @@
 
 namespace ButtonPollingSketch
 {
-    const unsigned long PIN_STRANGE = -1;
-    const unsigned long PIN_CHARM = -1;
+    const unsigned long PIN_STRANGE = 12;
+    const unsigned long PIN_CHARM = 13;
 
     constexpr char* STRANGE_TOPIC_NAME = "/buttons/strange";
     constexpr char* CHARM_TOPIC_NAME = "/buttons/charm";
@@ -18,6 +18,9 @@ namespace ButtonPollingSketch
     ros::Publisher charm_pub(CHARM_TOPIC_NAME, &charm_msg);
 
     void setup() {
+        Global::nh.advertise(strange_pub);
+        Global::nh.advertise(charm_pub);
+        
         pinMode(PIN_STRANGE, INPUT_PULLUP);
         pinMode(PIN_CHARM, INPUT_PULLUP);
     }
