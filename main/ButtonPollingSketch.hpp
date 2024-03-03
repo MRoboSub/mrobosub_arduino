106241004fab546142569a5bb72464fbe162d70f
elainamn <elainamn@umich.edu>
2024-03-03 20:07:12 +0000

    Added LimitSwitchSketch
    Proof of concept for the limit switch on Arduino

--- main/ButtonPollingSketch.hpp
+++ main/ButtonPollingSketch.hpp
@@ -38,3 +38,22 @@ namespace ButtonPollingSketch
         charm_pub.publish(&charm_msg);
     }
 }
+
+namespace LimitSwitchSketch 
+{
+    #define BUTTON_PIN 38
+
+    void setup(){
+
+    Serial.begin(9600);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    
+    }
+
+    void loop(){
+
+    Serial.println(digitalRead(BUTTON_PIN));
+    delay(100);
+
+    }
+}
